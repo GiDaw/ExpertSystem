c72708c001627b6853f544aabb3a0d21d436a5d8
GiDaw <driftkompas@wp.pl>
2024-12-27 15:04:20 +0100

    python script integration part 1

--- app/src/App.js
+++ app/src/App.js
@@ -6,13 +6,17 @@ import axios from 'axios';
 
 function App() {
   const [question, setQuestion] = useState({
-    "question": "Jak ci mija dzien",
-    "answers": ["slabo","normalnie","dobrze"],
-    "image": "obrazeksrc",
-    "description" : "Jak dzisiaj sie cos stalo to mozesz mi powiedziec"
+    'question': 'Wybierz gatunek gry.',
+    'answers': [{'name':'RPG','id':0, 'description': 'Gry w których gramy jedna lub drużyna postaci, rozwijając naszą postać czy drużyne w walce i poznając świat z ich strony'}, 
+                {'name':'RoqueLike','id':1,'description':'Gry z randomowymi wygenorowanymi poziomamy przez co każda rozgrywka jest inna'}, 
+                {'name':'RTS','id':2,'description':'Gry w których wcialmy się w dowódce miasta czy armii i musimy użyc strategii aby pokonać przeciwnika'} ],
+    'image': 'Gatunek.jpg',
+    'description' : 'Jaki gatunek gry chcesz zagrac?'
   })
   const [questionId, setQuestionId] = useState(0)
-
+  const [answerId, setAnswerId] = useState(-1)
+  const [description, setDescription] = useState(question.description)
+  const [image, setImage] = useState(question.image)
   const serwerUrl="http://localhost:5000/"
 
 
@@ -21,21 +25,52 @@ function App() {
       await axios.get(serwerUrl+"question/"+questionId.toString())
       .then(function (response){
         setQuestion(response.data);
+        setDescription(response.data.description)
       })      
       .catch(function (error) {
         console.log(error);
       });
 
   }
+  async function send_answer(answerId) {
+      
+    await axios.post(serwerUrl+"question/"+questionId.toString(),{
+      'answer' : answerId
+    })
+    .then(function (response){
+      setQuestion(response.data);
+      setDescription(response.data.description)
+    })      
+    .catch(function (error) {
+      console.log(error);
+    });
 
-  const button_click = (e)=>{
+}
+
+
+  const button_click = (e,id)=>{
     e.preventDefault();
     setQuestionId(questionId+1)
-    get_question()
+    send_answer(answerId)
+  }
+  const button_hover = (e,id)=>{
+    if (id == -1){
+      setDescription(question.description)
+      setImage(question.image)
+    }
+    else{
+      setAnswerId(id)
+      setDescription(question.answers[id]['description'])
+      setImage(question.answers[id]['image'])
+    }
   }
 
-  var answers = question.answers.map((q) =>
-    <button className="answerBtn" onClick={button_click}>{q}</button>
+  var answers = question.answers.map((a) =>
+    <button className="answerBtn" 
+    onMouseLeave={e=>button_hover(e,-1)} 
+    onMouseEnter={e=>button_hover(e,a['id'])} 
+    onClick={e=>button_click(e,a['id'])}>
+    {a['name']}</button>
   );
 
   useEffect (()=>{
@@ -48,8 +83,9 @@ function App() {
       <a src={question.image}></a>
       <h1>{question.question}</h1>
       {answers}
-      <p>Wytłumaczenie:{question.description}</p>
-
+      <p>Wytłumaczenie:</p>
+      <p>{description}</p>
+      <img src={serwerUrl+"static/images/"+image}></img>
       </div>
     </div>
   );
--- serwer/index.py
+++ serwer/index.py
@@ -1,32 +1,145 @@
-from flask import Flask
+from flask import Flask,send_file,request
 from flask_cors import CORS
+import pandas
+from sklearn import tree
+
+from sklearn.tree import DecisionTreeClassifier
+import matplotlib.pyplot as plt
+import matplotlib.image as pltimg
+
+df = pandas.read_csv('./CzyZagram.csv',sep=";")
+
+
+
+d = {'RPG': 0, 'RoqueLike': 1, 'RTS': 2}
+df['Gatunek'] = df['Gatunek'].map(d)
+
+d = {'turowa': 0, 'akcji': 1, 'shooter':2,
+     'platform':3,'autoBattler':4, 'baseBuilding':5}
+df['Typ'] = df['Typ'].map(d)
+
+d = {'rozwinieta': 0, 'prosta': 1}
+df['Historia'] = df['Historia'].map(d)
+
+d = {'dobre': 0, 'slabe': 1}
+df['Sterowanie'] = df['Sterowanie'].map(d)
+
+d = {'dluga': 0, 'krotka': 1}
+df['Dlugosc'] = df['Dlugosc'].map(d)
+
+d = {'zagram': 1, 'nie zagram': 0}
+df['Go'] = df['Go'].map(d)
+
+features = ['Gatunek', 'Typ','Historia' ,'Sterowanie', 'Dlugosc']
+
+X = df[features]
+y = df['Go']
+
+dtree = DecisionTreeClassifier()
+dtree = dtree.fit(X, y)
+
+inputs = [[]]
+
+# print(dtree.predict([[0, 0, 0, 0, 0]]))
+
+
+
+# data = tree.export_graphviz(dtree, out_file=None, feature_names=features)
+# graph = pydotplus.graph_from_dot_data(data)
+# graph.write_png('mydecisiontree.png')
+
+# img=pltimg.imread('mydecisiontree.png')
+# imgplot = plt.imshow(img)
+# plt.show()
+
 
 questions = [{
-    "question": "Jak ci mija dzien",
-    "answers": ["slabo","normalnie","dobrze"],
-    "image": "obrazeksrc",
-    "description" : "Jak dzisiaj sie cos stalo to mozesz mi powiedziec"
-  },{
-    "question": "Co jadleś",
-    "answers": ["banana","płatki","ziemniaki"],
-    "image": "obrazeksrc",
-    "description" : "Co miałes na śnadanie?"
+    'question': 'Wybierz gatunek gry.',
+    'answers': [{'name':'RPG','id':0,'image':'RPG.jpg', 'description': 'Gry w których gramy jedna lub drużyna postaci, rozwijając naszą postać czy drużyne w walce i poznając świat z ich strony'}, 
+                {'name':'RoqueLike','id':1,'image':'RoqueLike.jpg','description':'Gry z randomowymi wygenorowanymi poziomamy przez co każda rozgrywka jest inna'}, 
+                {'name':'RTS','id':2, 'image':'RTS.jpg','description':'Gry w których wcialmy się w dowódce miasta czy armii i musimy użyc strategii aby pokonać przeciwnika'} ],
+    'image': 'Gatunek.jpg',
+    'description' : 'Jaki gatunek gry chcesz zagrac?'
+  },
+  {
+    'question': 'Wybierz typ gry',
+    'image': 'Types.jpg',
+    'description' : 'Wybierz typ gatunku jakiego wybrałes wcześniej'
+  },
+  {
+    'question': 'historia gry',
+    'answers':[{'name':'rozwinieta','id': 0,'image':'Advanced.jpg','description':'Historia gry ma wiele wątków pobocznych i jest wciągająca'},
+               {'name':'prosta','id': 1,'image':'Simple.jpg','description':'Historia gry ma tylko wątek głowny albo nie ma jej wcale'}],
+    'image': 'History.jpg',
+    'description' : 'Jak bardzo bedzie rozwinięta historia gry?'
+  },
+  {
+    'question': 'Sterowanie gry',
+    'answers':[{'name':'dobre','id': 0,'image':'Good.jpg','description':'Sterowanie jest wygodne i jest bardzo elastyczne'},
+               {'name':'slabe','id': 1,'image':'Bad.jpg','description':'Sterowanie przyszkadza w rozgrywce i nie da się go zmienić'}],
+    'image': 'Controls.jpg',
+    'description' : 'Czy w grze sie fajnie porusza?'
+  },
+  {
+    'question': 'Dlugosc gry',
+    'answers':[{'name':'dluga','id': 0,'image':'Long.jpg','description':'Gra jest nie skończona albo Przejście całej gry trwa więcej niż 20h'},
+               {'name':'krotka','id': 1,'image':'Short.jpg','description':'Przejście całej gry trwa mnie niż 20h'}],
+    'image': 'HowLong.jpg',
+    'description' : 'jak długo da się grać w daną gre'
+  },  
+  {
+    'question': 'Finalny wynik',
+    'answers':[{'name':'dobre','id': 0,'image':'Good.jpg','description':'Historia gry ma wiele wątków pobocznych i jest wciągająca'},
+               {'name':'slabe','id': 1,'image':'Bad.jpg','description':'Historia gry ma tylko wątek głowny albo nie ma jej wcale'}],
+    'image': 'History.jpg',
+    'description' : 'Czy w grze sie fajnie porusza?'
   }]
 
 
 app = Flask(__name__)
 CORS(app)
 
-app.after_request
-def after_request(response):
-    header = response.headers
-    header['Access-Control-Allow-Origin'] = '*'
-    return response
     
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
-@app.route('/question/<int:questionId>',methods=['GET'])
+@app.route("/image/<name>", methods=['GET'])
+def get_image(name):
+    return send_file('./images'+name,mimetype='image/jpg')
+
+@app.route('/question/<int:questionId>',methods=['GET','POST'])
 def get_question(questionId):
-    return questions[questionId]    
+     if request.method == 'GET':
+        return questions[questionId]
+     if request.method == 'POST':
+        
+        body=request.json
+        inputs[0].append(body['answer'])
+
+        if (questionId == 1):
+            answers=[{'name':'turowa','id':0,'image':'TurnBased.jpg','description':'Gra rpg w których walki rozgrywają się turowo na zmiane'},
+                {'name':'akcji','id':1,'image':'Action.jpg','description':'Gra rpg w których walki so szybkie i rozgrywają się w czasie rzeczywistym'},
+                {'name':'shooter','id':2,'image':'Shooter.jpg','description':'Gry Roquelike w których głownie strzela się z daleka do przeciwników przez to wymagają one od gracza prezycji'},
+                {'name':'platform','id':3,'image':'Platformer.jpg','description':'Gry Roquelike w których skacze się miedzy różnymi przeszkodami aby dość do jakiegos celu'},
+                {'name':'autoBattler','id':4,'image':'AutoBattler.jpg','description':'Gry RTS w której walka rozgrywa się sama my tylko wymyślamy strategie naszej armii'},
+                {'name':'baseBuilding','id':5,'image':'BaseBuilding.jpg','description':'Gry RTS w której musimy zbudować dobrą baze z której pożniej bedziemy wysyłać jednostki do przeciwnika'}]        
+            # 'akcji', 'shooter',
+            #       'platform','autoBattler', 'baseBuilding'],
+
+            if(body['answer'] == 0):
+              questions[questionId]['answers']=[answers[0],answers[1]]
+            if(body['answer'] == 1):
+              questions[questionId]['answers']=[answers[2],answers[3]]
+            if(body['answer'] == 2):
+              questions[questionId]['answers']=[answers[4],answers[5]]
+
+            print(questions)
+        return questions[questionId] 
+
+
+
+
+
+
+
